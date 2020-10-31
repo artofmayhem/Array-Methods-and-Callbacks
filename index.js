c6fa3225b85f6e3d019a20916b7a367a1863dc00
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-10-30 20:56:07 -1000

    problems 1-3 complete
    may need to redo function 3 because it's solved using closure instead of callback to arrive at the same result

--- index.js
+++ index.js
@@ -13,23 +13,60 @@ console.log('its working');
 (e) Winner of 2014 world cup final */
 
 
+
+
+console.log(fifaData[828]["Home Team Name"]);           // expect  Germany
+console.log(fifaData[828]["Away Team Name"]);           // expect Argentina
+console.log(fifaData[828]["Home Team Goals"]);          // expect 1
+console.log(fifaData[828]["Away Team Goals"]);          // expect 0
+console.log(fifaData[828]["Win conditions"]);           // expect "Germany win after extra time"
+
+
+
+
+
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
 
-    /* code here */
 
-};
+
+
+let finals = [];
+
+function getFinals(data)                                                //function declaration taking in data as a parameter
+{
+finals = data.filter(   element  =>  element.Stage === 'Final'  );      // data cycler arrow function
+       return finals;
+}; 
+
+console.log(getFinals(fifaData));                                       //function invocation using fifaData as argument
+
+
+
+
+
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
 
-    /* code here */
 
+
+
+
+function getYears(data) 
+{                           
+    let yearResults = [];
+    data.forEach( element => {
+        yearResults.push(element.Year );
+    });
+      console.log(yearResults)      
 };
 
-getYears();
+getYears(finals);   
+
+
+
+
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
